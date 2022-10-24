4e639948c82742f29991bf532e21c9b6372473d0
Ana Martinez <anaisabelmarba@gmail.com>
2022-10-24 13:54:28 -0300

    inicio de modelos

--- ProyectoFinalBlog/blog/models.py
+++ ProyectoFinalBlog/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Inicio(models.Model):
+    pass
+
+class About(models.Model):
+    Nosotros = models.CharField
+    descripción = models.CharField
+    
+
+
+    
